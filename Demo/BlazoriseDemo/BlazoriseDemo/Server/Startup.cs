26a6a23f6db30c7442e4c92efb6ab575adc5a85c
conan77 <panfeng77@126.com>
2019-10-15 17:37:51 +0800

    修改

--- Demo/BlazoriseDemo/BlazoriseDemo/Server/Startup.cs
+++ Demo/BlazoriseDemo/BlazoriseDemo/Server/Startup.cs
@@ -5,11 +5,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BlazoriseDemo.Server
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,6 +28,8 @@ namespace BlazoriseDemo.Server
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
                     new[] { "application/octet-stream" });
             });
+            var connectString = _config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<NorthwindContext>(options=>options.UseSqlServer(connectString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
