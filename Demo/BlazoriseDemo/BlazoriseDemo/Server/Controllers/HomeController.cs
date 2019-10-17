b63fddeb7c08e4bfa0161bbbbd7bcc1e06d8ce25
conan77 <panfeng77@126.com>
2019-10-17 15:31:01 +0800

    add loading

---
+++ Demo/BlazoriseDemo/BlazoriseDemo/Server/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazoriseDemo.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BlazoriseDemo.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> logger;
+        private readonly _182810Context _context;
+
+        public HomeController(ILogger<HomeController> logger,
+            _182810Context context)
+        {
+            this.logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            logger.Log(LogLevel.Information,"home api connect server");
+            var data = _context.BaseSystemConfig.ToList();
+            return string.Empty;
+        }
+    }
+}
