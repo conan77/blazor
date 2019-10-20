1ae4b27435e92c8c5657aa58fb6fc010c0417647
conan77 <panfeng77@126.com>
2019-10-20 20:49:36 +0800

    dapper

---
+++ Demo/com.caimomo.hudan/com.caimomo.hudan/Server/SqlServerRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace com.caimomo.Dapper.Base
+{
+    /// <summary>
+    /// 基类业务接口定义
+    /// </summary>
+    public class SqlServerRepository<TEntity> where TEntity : IEntity , IRepositoryBase
+    {
+        public SqlServerRepository(SqlConnection dbConnection)
+        {
+            this.DbConnection = dbConnection;
+        }
+        public SqlServerRepository(string connectionString)
+        {
+            this.DbConnection = new SqlConnection(connectionString);
+        }
+
+        public string ConnectionString => DbConnection?.ConnectionString ?? string.Empty;
+
+        public RepositoryBaseType DatabaseType => RepositoryBaseType.Sqlserver;
+
+        public IDbConnection DbConnection { get; set; }
+        public TEntity GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IEntity> GetAll()
+        {
+            var tableName = nameof(TEntity);
+            DbConnection.
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+
+        }
+
+        public void Insert(IEnumerable<TEntity> entities)
+        {
+        }
+
+        public void Update(TEntity entity)
+        {
+        }
+
+        public void Update(IEnumerable<TEntity> entities)
+        {
+        }
+
+        public void Delete(TEntity entity)
+        {
+        }
+
+        public void Delete(IEnumerable<TEntity> entities)
+        {
+        }
+    }
+}
